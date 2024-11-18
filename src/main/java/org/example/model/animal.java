0b3502ddc1c2595c9076217365d271238d430cfd
Ahmed BOURAZZA <e2405908@ensimlan.univ-lemans.fr>
2024-11-18 16:13:37 +0100

    first commit

---
+++ src/main/java/org/example/model/animal.java
@@ -0,0 +1,18 @@
+package org.example.model;
+
+public class animal {
+    String nomAnimal;
+    TypeAnimal typeAnimal;
+
+    public String getNomAnimal() {
+        return nomAnimal;
+    }
+
+    public TypeAnimal getTypeAnimal() {
+        return typeAnimal;
+    }
+}
+ enum TypeAnimal {
+    animal1, animal2, animal3, animal4, animal5, animal6, animal7,
+
+}
