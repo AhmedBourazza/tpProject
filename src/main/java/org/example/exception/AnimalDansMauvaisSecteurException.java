4ff86956691debd227c0bbacc9edcbfbfc79f4e7
Ahmed BOURAZZA <e2405908@ensimlan.univ-lemans.fr>
2024-11-18 16:30:35 +0100

    creation des exceptions

---
+++ src/main/java/org/example/exception/AnimalDansMauvaisSecteurException.java
@@ -0,0 +1,7 @@
+package org.example.exception;
+
+public class AnimalDansMauvaisSecteurException extends Exception {
+    public AnimalDansMauvaisSecteurException(String message) {
+        super(message);
+    }
+}
---
+++ src/main/java/org/example/exception/LimiteVisiteurException.java
@@ -0,0 +1,7 @@
+package org.example.exception;
+
+public class LimiteVisiteurException extends Exception {
+    public LimiteVisiteurException(String message) {
+        super(message);
+    }
+}
